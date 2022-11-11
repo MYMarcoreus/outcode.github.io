8e0a93137d9481c8644deec7c9140618477cd908
3119008477 <572260643@qq.com>
2022-11-11 22:40:04 +0800

    尝试使用cookie机制1.0

--- statics/cookie.js
+++ statics/cookie.js
@@ -2,15 +2,17 @@ function setCookie(ckey,cvalue, exdays=60){
     var d = new Date();
 	d.setTime(d.getTime()+(exdays*24*60*60*1000));
 	var expires = "expires="+d.toGMTString();
-    console.log( ckey+"="+cvalue+"; " + expires+"; " + "path=./");
-    if ( document.cookie == null || document.cookie == "" )
-        document.cookie = ckey   +"="+  encodeURIComponent(cvalue) +"; "
-                        + expires+"; "
-                        + "path=./";
-    else
-        document.cookie = ckey+"="+cvalue+"; " + expires+"; " + "path=./";
+
+    cookie_str =
+        ckey +"="+  encodeURIComponent(cvalue) +"; "+
+        expires+"; " +
+        "path=./"
+    console.log(cookie_str);
+    document.cookie = cookie_str
     console.log(document.cookie);
-    }
+}
+
+
 function getCookie(ckey){
     var key = ckey + "=";
     var ca = document.cookie.split(';');
